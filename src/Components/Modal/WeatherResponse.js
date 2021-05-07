887afd37d9e9f77ee136bbefd5ded3d9171205c5
suvitha-workspace <80394034+suvitha-workspace@users.noreply.github.com>
2021-05-07 18:13:58 +0530

    Create WeatherResponse.js

---
+++ src/Components/Modal/WeatherResponse.js
@@ -0,0 +1,185 @@
+export const WeatherResponse = {
+  weekDays: [
+    {
+      id: "1",
+      title: "Wed 05",
+      subTitle: 'WEDNESDAY, MAY 05',
+      tempMax: "34°",
+      tempMin: "28°",
+      canBeRain: false,
+      rangeId: 301,
+      wind: 'SSE 7 mph',
+      windGuts: '23mph',
+      thunderStorm: '0%',
+      cloudCover: "41%",
+      Precipitation: '4.00',
+      probPrecitation: '6%',
+      humidity: '78%',
+      dewPoint: "28°",
+      sunDetails:
+      {
+        mSunSet: '6:25 PM',
+        mSunRise: '5:47 AM',
+        eSunSet: '2:19 PM',
+        eSunRise: '2:19 AM',
+      }
+
+    },
+    {
+      id: "2",
+      title: "Thu 06",
+      subTitle: 'THURSDAY, MAY 06',
+      tempMax: "43°",
+      tempMin: "23°",
+      cloudDenses: "low",
+      canBeRain: true,
+      rangeId: 401,
+      wind: 'SSE 5 mph',
+      windGuts: '20mph',
+      thunderStorm: '0%',
+      cloudCover: "45%",
+      Precipitation: '8.00',
+      probPrecitation: '10%',
+      humidity: '68%',
+      dewPoint: "23°",
+      sunDetails:
+      {
+        mSunSet: '6:25 PM',
+        mSunRise: '5:47 AM',
+        eSunSet: '2:19 PM',
+        eSunRise: '2:19 AM',
+      }
+
+    },
+    {
+      id: "3",
+      title: "Fri 07",
+      tempMax: "42°",
+      subTitle: 'FRIDAY, MAY 07',
+      tempMin: "22°",
+      cloudDenses: "",
+      canBeRain: false,
+      rangeId: 500,
+      wind: 'ESE 7 mph',
+      windGuts: '25mph',
+      thunderStorm: '2%',
+      cloudCover: "39%",
+      Precipitation: '2.00',
+      probPrecitation: '8%',
+      humidity: '75%',
+      dewPoint: "24°",
+      sunDetails:
+      {
+        mSunSet: '6:25 PM',
+        mSunRise: '5:47 AM',
+        eSunSet: '2:19 PM',
+        eSunRise: '2:19 AM',
+      }
+
+    },
+    {
+      id: "4",
+      title: "Sat 08",
+      subTitle: 'SATURDAY, MAY 08',
+      tempMax: "41°",
+      tempMin: "21°",
+      cloudDenses: "",
+      canBeRain: false,
+      rangeId: 135,
+      wind: 'SSE 5 mph',
+      windGuts: '23mph',
+      thunderStorm: '3%',
+      cloudCover: "41%",
+      Precipitation: '5.00',
+      probPrecitation: '6%',
+      humidity: '71%',
+      dewPoint: "21°",
+      sunDetails: 
+        {
+          mSunSet: '6:18 PM',
+          mSunRise: '5:47 AM',
+          eSunSet: '2:19 PM',
+          eSunRise: '2:19 AM',
+        }
+      
+    },
+    {
+      id: "5",
+      title: "Sun 09",
+      subTitle: 'SUNDAY, MAY 09',
+      tempMax: "39°",
+      tempMin: "19°",
+      cloudDenses: "",
+      canBeRain: false,
+      rangeId: 444,
+      wind: 'ESE 7 mph',
+      windGuts: '23mph',
+      thunderStorm: '0%',
+      cloudCover: "41%",
+      Precipitation: '2.00',
+      probPrecitation: '6%',
+      humidity: '72%',
+      dewPoint: "28°",
+      sunDetails:
+      {
+        mSunSet: '6:25 PM',
+        mSunRise: '5:47 AM',
+        eSunSet: '2:19 PM',
+        eSunRise: '2:19 AM',
+      }
+
+    },
+    {
+      id: "6",
+      title: "Mon 10",
+      subTitle:'MONDAY, MAY 10',
+      tempMax: "38°",
+      tempMin: "18°",
+      cloudDenses: "heavy",
+      canBeRain: true,      
+      rangeId: 202,
+      wind: 'SSE 3 mph',
+      windGuts: '23mph',
+      thunderStorm: '0%',
+      cloudCover: "41%",
+      Precipitation: '3.00',
+      probPrecitation: '9%',
+      humidity: '78%',
+      dewPoint: "28°",
+      sunDetails:
+      {
+        mSunSet: '6:25 PM',
+        mSunRise: '5:47 AM',
+        eSunSet: '2:19 PM',
+        eSunRise: '2:19 AM',
+      }
+
+    },
+    {
+      id: "7",
+      title: "Tue 11",
+      subTitle: 'TUESDAY, MAY 11',
+      tempMax: "37°",
+      tempMin: "17°",
+      cloudDenses: "",
+      canBeRain: true,
+      rangeId: 501,
+      wind: 'SSE 7 mph',
+      windGuts: '23mph',
+      thunderStorm: '0%',
+      cloudCover: "41%",
+      Precipitation: '5.00',
+      probPrecitation: '6%',
+      humidity: '78%',
+      dewPoint: "28°",
+      sunDetails:
+      {
+        mSunSet: '6:25 PM',
+        mSunRise: '5:47 AM',
+        eSunSet: '2:19 PM',
+        eSunRise: '2:19 AM',
+      }
+
+    },
+  ],
+};
